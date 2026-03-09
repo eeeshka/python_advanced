26b8edea436ef78d2d2f0ee3fd1cc754bfb3024e
Владислав Антонов <nnbull2602@gmail.com>
2026-03-09 13:35:08 +0300

    Добавлена валидация email в класс User

--- src/models/user.py
+++ src/models/user.py
@@ -1,3 +1,5 @@
+import re
+
 class User:
     def __init__(self, name, email):
         self.name = name
@@ -5,3 +7,16 @@ class User:
     
     def get_info(self):
         return "Пользователь: " + self.name + ", Email: " + self.email
+
+
+    @property
+    def email(self) -> str:
+        return self._email
+
+    @email.setter
+    def email(self, new_email: str) -> None:
+        pattern: str = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'
+        if re.fullmatch(pattern, new_email):
+            self.email = new_email
+            return
+        raise ValueError('Неверный формат email')
